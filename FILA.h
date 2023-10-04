ee93569c2f20f319dc1f044e1c010d1f1aae8a80
xF3rd <102755227+xF3rd@users.noreply.github.com>
2023-10-04 15:38:16 -0300

    fiz a Adição de tarefa e a modificação alem de ter feito o imprime fila na opção 5

---
+++ FILA.h
@@ -0,0 +1,162 @@
+#ifndef FILA_H_INCLUDED
+#define FILA_H_INCLUDED
+#include <string.h>
+
+/* FUNÇÕES DE MANIPULAÇÃO DE PFILA
+
+Fila* CriaFila()  CRIA A FILA
+
+int VaziaFila (Fila* f) VERIFICA SE A FILA ESTÁ VAIZA
+
+void InsereFila (Fila* f, int v) INSERÇÃO
+
+int RetiraFila (Fila* f) REMOÇÃO
+
+Fila* liberaFila (Fila* f) LIBERA A FILA
+
+void imprimeFila (Fila* f)IMPRIME A FILA
+*/
+
+typedef struct data
+{
+    int dia;
+    int mes;
+    int ano;
+
+}data;
+typedef struct nos
+{
+    int codigo;
+    char nome[30];
+    char proj[30];
+    data inicio;
+    data fim;
+    int status;
+    struct nos *prox;
+}No;
+
+typedef struct fila
+{
+    No * ini;
+    No * fim;
+} Fila;
+
+int VaziaFila (Fila* f)
+{
+    if (f->ini==NULL) return 1;
+    return 0;
+
+}
+
+
+Fila* CriaFila ()
+{
+    Fila* f = (Fila*) malloc(sizeof(Fila));
+    f->ini = f->fim = NULL;
+    return f;
+}
+
+No* ins_fim (No* fim, int A,char b[],char c[], data d, data e)
+{
+    No* p = (No*) malloc(sizeof(No));
+    p->codigo = A;
+    strcpy(p->nome,b);
+    strcpy(p->proj,c);
+    p->inicio = d;
+    p->fim = e;
+    p->status = 0;
+    p->prox = NULL;
+    if (fim != NULL) /* verifica se lista não estava vazia */
+    fim->prox = p;
+    return p;
+}
+
+void InsereFila (Fila* f, int a,char *b,char *c, data d, data e)
+{
+    f->fim = ins_fim(f->fim,a,b,c,d,e);
+    if (f->ini==NULL) /* fila antes vazia? */
+    f->ini = f->fim;
+}
+
+No* retira_ini (No* ini)
+{
+    No* p = ini->prox;
+    free(ini);
+    return p;
+}
+
+No* RetiraFila (Fila* f)
+{
+    No* v;
+    if (VaziaFila(f))
+    {
+        printf("Fila vazia.\n");
+        exit(0); /* aborta programa */
+    }
+    v = f->ini;
+    f->ini = retira_ini(f->ini);
+    if (f->ini == NULL) /* fila ficou vazia? */
+    f->fim = NULL;
+    return v;
+}
+
+void imprimeData (data f){
+
+    printf("%d/%d/%d",f.dia,f.mes,f.ano);
+}
+
+
+
+void imprimeFila (Fila* f)
+{
+    No* q;
+    printf("\n\t\t");
+    for (q=f->ini; q!=NULL; q=q->prox)
+    {
+        printf("\ncodigo:%d",q->codigo);
+        printf("\nNome da tarefa:%s - ",q->nome);
+        printf("\nNome do projeto:%s - ",q->proj);
+        printf("\ninicio:");
+        imprimeData(q->inicio);
+        printf("\nfim:");
+        imprimeData(q->fim);
+        printf("\nStatus:%d - ",q->status);
+
+    }
+    printf("\n");
+}
+
+
+Fila* liberaFila (Fila* f)
+{
+    No* q = f->ini;
+    while (q!=NULL)
+    {
+        No* t = q->prox;
+        free(q);
+        q = t;
+    }
+    free(f);
+    return NULL;
+}
+
+void ModificaTarefa(Fila* f,int codigo,int nVal,char *nome,char *proj, data DataIni, data DataFim){
+    No* atual = f->fim;
+    int contador = 0;
+
+    while(atual !=NULL){
+        if(contador == codigo){
+            atual->codigo = nVal;
+            strcpy(atual->nome,nome);
+            strcpy(atual->proj,proj);
+            atual->inicio = DataIni;
+            atual->fim = DataFim;
+            return;
+        }
+        atual = atual->prox;
+    }
+
+
+}
+
+#endif // FILA_H_INCLUDED
---
+++ main.c
@@ -0,0 +1,148 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include "FILA.h"
+
+
+int main()
+{
+setlocale(LC_ALL, "Portuguese");
+int menu;
+int codigo,novoCodigo;
+char nome[30],proj[30];
+Fila *tarefa;
+data dataIni;
+data dataFim;
+
+tarefa = CriaFila();
+
+do{
+    printf("Digite o número referente a funcionalidade desejada:\n");
+    printf("1 - Adicionar Tarefa\n");
+    printf("2 - Modificar Tarefa\n");
+    printf("3 - Concluir Tarefa\n");
+    printf("4 - Atualização do Status\n");
+    printf("5 - Tarefas Pendentes\n");
+    printf("6 - Tarefas Concluidas\n");
+    printf("7 - Tarefas Concluídas com e sem Atrasos\n");
+    printf("8 - Sair do Programa\n");
+    scanf("%d",&menu);
+
+    switch(menu){
+
+            case 1:
+
+                /*Adiciona uma tarefa a fila de tarefas*/
+
+
+                printf("\nOpção 1 escohida\n");
+
+                printf("Qual o codigo: ");
+                scanf("%d",&codigo);
+
+                printf("Qual o nome da tarefa: ");
+                fflush(stdin);
+                gets(nome);
+
+                printf("Qual o projeto da tarefa: ");
+                fflush(stdin);
+                gets(proj);
+
+                printf("Qual o dia de inicio da tarefa: ");
+                scanf("%d",&dataIni.dia);
+
+                printf("Qual o mes de inicio da tarefa: ");
+                scanf("%d",&dataIni.mes);
+
+                printf("Qual o ano de inicio da tarefa: ");
+                scanf("%d",&dataIni.ano);
+
+                printf("Qual o dia de fim da tarefa: ");
+                scanf("%d",&dataFim.dia);
+
+                printf("Qual o mes de fim da tarefa: ");
+                scanf("%d",&dataFim.mes);
+
+                printf("Qual o ano de fim da tarefa: ");
+                scanf("%d",&dataFim.ano);
+
+                InsereFila(tarefa,codigo,nome,proj,dataIni,dataFim);
+                break;
+
+            case 2:
+                printf("\nOpção 2 escohida\n");
+
+                printf("Qual o codigo para ser modificado: ");
+                scanf("%d",&codigo);
+
+                printf("Qual o novo codigo: ");
+                scanf("%d",&novoCodigo);
+
+                printf("Qual o novo nome: ");
+                fflush(stdin);
+                gets(nome);
+
+                printf("Qual o novo nome do projeto: ");
+                fflush(stdin);
+                gets(proj);
+
+                printf("Qual o novo dia de inicio da tarefa: ");
+                scanf("%d",&dataIni.dia);
+
+                printf("Qual o novo mes de inicio da tarefa: ");
+                scanf("%d",&dataIni.mes);
+
+                printf("Qual o novo ano de inicio da tarefa: ");
+                scanf("%d",&dataIni.ano);
+
+                printf("Qual o novo dia de fim da tarefa: ");
+                scanf("%d",&dataFim.dia);
+
+                printf("Qual o novo mes de fim da tarefa: ");
+                scanf("%d",&dataFim.mes);
+
+                printf("Qual o novo ano de fim da tarefa: ");
+                scanf("%d",&dataFim.ano);
+
+
+
+                ModificaTarefa(tarefa,codigo,novoCodigo,nome,proj,dataIni,dataFim);
+
+
+                break;
+
+            case 3:
+                printf("\nOpção 3 escohida\n");
+                break;
+
+            case 4:
+                printf("\nOpção 4 escohida\n");
+                break;
+
+
+            case 5:
+                printf("\nOpção 5 escohida\n");
+
+                imprimeFila(tarefa);
+                break;
+
+            case 6:
+                printf("\nOpção 6 escohida\n");
+                break;
+
+            case 7:
+                printf("\nOpção 7 escohida\n");
+                break;
+
+            case 8:
+                printf("\nOpção 8 escohida\n");
+                break;
+
+            default:
+                printf("\nDigite uma opcao valida\n");
+        }
+    }
+   while(menu);
+
+        return 0;
+}
